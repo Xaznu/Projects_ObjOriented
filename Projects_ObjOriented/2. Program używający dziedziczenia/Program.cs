52f5330f253212f06885931cf56d27b25c2ed363
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-13 22:24:32 +0200

    2. Program używający dziedziczenia

---
+++ Projects_ObjOriented/2. Program używający dziedziczenia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto sam = new Specyfikacja("Sportowe", 1200, 2000, 250, "Niebieski", 2);
+            sam.WyswietlSpecyfikacje();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Projects_ObjOriented/2. Program używający dziedziczenia/SpecyfikacjaAuto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie
+{
+    class Specyfikacja : Auto
+    {
+        public Specyfikacja(string kP, int w, int poj, int mS, string k, int ilOsi) : base(kP, w, poj, mS, k, ilOsi)
+        {
+
+        }
+
+        public void WyswietlSpecyfikacje()
+        {
+            base.WyswietlSpecyfikacje();
+        }
+    }
+}
