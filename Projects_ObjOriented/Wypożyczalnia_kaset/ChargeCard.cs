384eb14f6623a84367d79266c0e4f0470d0444f1
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-08 20:04:03 +0200

    #6 Payment class

---
+++ Projects_ObjOriented/Wypożyczalnia_kaset/ChargeCard.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp30
+{
+    public class Karta
+    {
+        public decimal kwota;
+        private readonly int kod;
+
+        public Karta()
+        {
+            this.kod = 100;
+            this.kwota = 1000;
+        }
+
+        public bool KartaAktywna(int kod1)
+        {
+            if (kod == kod1)
+                return true;
+            else return false;
+        }
+    }
+}
+
