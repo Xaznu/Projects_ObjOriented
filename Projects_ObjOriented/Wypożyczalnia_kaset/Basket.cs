ea1a31c76696fa55d1c75ce7348574674ebe2f77
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-08 20:04:37 +0200

    #7 Items class

---
+++ Projects_ObjOriented/Wypożyczalnia_kaset/Basket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp30
+
+{
+    public class Basket
+    {
+        public List<Item> ItemsInBasket;
+
+        public decimal BasketValue
+        {
+            get
+            {
+                decimal basketval = 0;
+                foreach (var item in ItemsInBasket)
+                {
+                    basketval += item.Price;
+                }
+                return basketval;
+            }
+            set { }
+        }
+
+        public int ItemCount
+        {
+            get
+            {
+                int itemcount = 0;
+                foreach (var item in ItemsInBasket)
+                {
+                    itemcount++;
+                }
+                return itemcount;
+            }
+            set { }
+        }
+
+        public Basket()
+        {
+            this.ItemsInBasket = new();
+        }
+        public void AddToBasket(Item i)
+        {
+            ItemsInBasket.Add(i);
+        }
+
+    }
+}
