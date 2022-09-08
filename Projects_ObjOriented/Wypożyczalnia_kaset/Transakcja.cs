822a2908930dc71cddf31987786d609539d36713
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-08 19:58:50 +0200

    #2 Utworzenie klasy transakcja

---
+++ Projects_ObjOriented/Wypożyczalnia_kaset/Transakcja.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp30
+{
+    public class Transakcja
+    {
+        public Customer customer;
+        public DateTime DataZakupu;
+        public decimal KwotaZamowienia;
+        public int iloscproduktow;
+        public List<Item> _listaprzedmiotów_zamówienia;
+        public readonly int numer_zamówienia;
+
+
+
+        public Transakcja(decimal kwotaZamowienia, int r)
+        {
+
+            this.KwotaZamowienia = kwotaZamowienia;
+            this.DataZakupu = DateTime.Now;
+            this._listaprzedmiotów_zamówienia = new();
+            this.numer_zamówienia = r;
+
+        }
+
+
+        public void PokażPrzedmioty()
+        {
+            Console.WriteLine("Lista przedmiotów: ");
+            foreach (var item in _listaprzedmiotów_zamówienia)
+            {
+                Console.WriteLine($"{item.title} {item.Price}, {item.type}, {item.czastrwania} ");
+            }
+        }
+
+
+
+    }
+}
